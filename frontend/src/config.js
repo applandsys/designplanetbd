29937650d0eb07441b411b49ebfc75d1ca449f1a
Soft Expert Zone <applandsys@gmail.com>
2025-12-25 16:43:34 +0600

    Fix (Frontend): Changed the config

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,6 +1,6 @@
 const config = {
-    apiBaseUrl: process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:4000/v1",
-    publicPath: process.env.NEXT_PUBLIC_PATH || "http://localhost:4000"
+    apiBaseUrl: process.env.NEXT_PUBLIC_API_BASE_URL || "https://backend.designplanetbd.com/v1",
+    publicPath: process.env.NEXT_PUBLIC_PATH || "https://backend.designplanetbd.com"
 };
 
 export default config;
