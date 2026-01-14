63ca6186795dbeca3a5e14f9aea47dfb5c72031c
Soft Expert Zone <applandsys@gmail.com>
2026-01-14 21:52:32 +0600

    Frontend: Promobox Margin button

--- frontend/src/components/ecommerce/PromoBox.jsx
+++ frontend/src/components/ecommerce/PromoBox.jsx
@@ -46,7 +46,7 @@ const PromoCards = () => {
                 <div className="absolute bottom-4 left-10">
                     {
                         banner.url && (
-                            <Link className="bg-green-500 hover:bg-green-600 text-white px-4 py-2 rounded-md text-sm w-max " href={banner.url}>
+                            <Link className="bg-green-500 hover:bg-green-600 text-white px-4 py-2 rounded-md text-sm w-max ml-20" href={banner.url}>
                                 Shop Now →
                             </Link>
                         )
