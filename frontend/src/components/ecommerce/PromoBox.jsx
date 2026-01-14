46c11d00fd828fbb7936c4a17ecd486df89a68d3
Soft Expert Zone <applandsys@gmail.com>
2026-01-14 21:55:08 +0600

    Frontend: Promobox positionleft

--- frontend/src/components/ecommerce/PromoBox.jsx
+++ frontend/src/components/ecommerce/PromoBox.jsx
@@ -43,7 +43,7 @@ const PromoCards = () => {
                     height={300} 
                     className="h-full object-contain mx-auto rounded-xl"
                 />
-                <div className="absolute bottom-4 left-10">
+                <div className="absolute bottom-4 left-20">
                     {
                         banner.url && (
                             <Link className="bg-green-500 hover:bg-green-600 text-white px-4 py-2 rounded-md text-sm w-max ml-20" href={banner.url}>
