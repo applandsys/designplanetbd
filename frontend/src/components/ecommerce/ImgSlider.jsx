1c9cd2316fe87eaf5fd880a0a35bc5b4131c1ddc
Soft Expert Zone <applandsys@gmail.com>
2026-01-14 15:46:42 +0600

    Frontend: sub text color

--- frontend/src/components/ecommerce/ImgSlider.jsx
+++ frontend/src/components/ecommerce/ImgSlider.jsx
@@ -112,7 +112,11 @@ export default function ImgSlider() {
                                     : "translate-x-[-50%] opacity-70"
                             }`}
                         >
-                            <h2 className={`text-base sm:text-lg md:text-xl  max-w-md mb-6 ${index === 0 ? "text-black" : "text-gray-300"}`}>
+                            <h2
+                                className={`text-base sm:text-lg md:text-xl max-w-md mb-6 ${
+                                    index === 0 && index === current ? "text-black" : "text-gray-600"
+                                }`}
+                            >
                                 {item.sub_text}
                             </h2>
                         </div>
