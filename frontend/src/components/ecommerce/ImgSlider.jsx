450b8ce31e96056f6f22fd9fa7c82b05e13a7132
Soft Expert Zone <applandsys@gmail.com>
2026-01-22 19:43:55 +0600

    Fix: slider mobile text size

--- frontend/src/components/ecommerce/ImgSlider.jsx
+++ frontend/src/components/ecommerce/ImgSlider.jsx
@@ -97,7 +97,7 @@ export default function ImgSlider() {
                             }`}
                         >
                             <h1
-                                className="text-xl md:text-4xl lg:text-6xl font-bold drop-shadow-lg mb-2 text-center md:text-left lg:text-left w-full text-black"
+                                className="text-md md:text-4xl lg:text-6xl font-bold drop-shadow-lg mb-2 text-center md:text-left lg:text-left w-full text-black"
                             >
                                 {item.title_text}
                             </h1>
@@ -111,7 +111,7 @@ export default function ImgSlider() {
                             }`}
                         >
                             <h2
-                                className="ext-base text-md sm:text-xl md:text-xl max-w-md mb-6 text-center md:text-left lg:text-left  text-black"
+                                className="ext-base text-[15px] sm:text-xl md:text-xl max-w-md mb-6 text-center md:text-left lg:text-left  text-black"
                             >
                                 {item.sub_text}
                             </h2>
