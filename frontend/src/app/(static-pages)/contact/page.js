cca8193eff5a94250c896e7f9fbdd9883d8d2e12
Soft Expert Zone <applandsys@gmail.com>
2026-01-23 16:06:16 +0600

    frontend: static page added

--- frontend/src/app/(static-pages)/contact/page.js
+++ frontend/src/app/(static-pages)/contact/page.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import Footer from "@/components/theme/Footer";
+import Header from "@/components/layouts/Header";
+import { ReduxProvider } from '@/providers/ReduxProvider';
+import ContactForm from "@/components/static/ContactForm";
+import ContactHero from "@/components/static/ContactHero";
+
 
 const ContactPage = () => {
     return (
-        <div>
-            CONTACT US
-        </div>
+        <ReduxProvider>
+            <Header />
+            <ContactHero/>
+            <ContactForm />
+            <Footer />
+        </ReduxProvider>
     );
 };
 
