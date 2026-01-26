219d3ed8e3e1a213523edcb79e8ee9fd3730f398
Soft Expert Zone <applandsys@gmail.com>
2026-01-26 18:23:04 +0600

    frontend: Cart list text color

--- frontend/src/app/(ecommerce-front)/auth/login/page.jsx
+++ frontend/src/app/(ecommerce-front)/auth/login/page.jsx
@@ -3,11 +3,13 @@ import Login from "@/components/auth/Login";
 
 export default  function LoginPage(props) {
     return (
-        <div className="min-h-screen flex items-center justify-center">
-            <div className="w-full max-w-md   ">
-                <h2 className="text-2xl font-bold text-center text-gray-800 mb-1">Sign in</h2>
-                <Login type="customer" />
-            </div>
+    <div className="min-h-screen flex justify-center pt-16 lg:pt-12">
+        <div className="w-full max-w-md">
+            <h2 className="text-2xl font-bold text-center text-gray-800 mb-1">
+                Sign in
+            </h2>
+            <Login type="customer" />
         </div>
+    </div>
     );
 }
